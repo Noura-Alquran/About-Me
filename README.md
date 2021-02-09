cf2a9d6e5029deefdd4874f97768ddeee9bbdff2
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-02-09 23:23:28 +0200

    lab03

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # About-Me
-“About Me” project 201
+##“About Me” project 201 
+* In lab 02 I created an ***About Me guessing game*** that utilizes HTML, CSS, & JavaScript.
+   * In HTML file I bulid the HTML code include a short biography using < p >,  education history (unordered list), and table which contain my skills.
+   * In CSS file I added stylations to vary different parts of html 
+   * In javaScript file I created five yes/no questions using prompt so the user can enter him/her answar then using .toLowerCase(); to make all possible inputs validated.
+
+* In lab03 I modified the HTML file by adding ordered list of 10 goals to set in 2021.Also ,I added addtional CSS to style my HTML page.
+  * In javaScript 
+    1.I added a 6th question that takes in a numeric input by prompting the user to guess my favorite number. I used *if statement* to Indicates through an alert if the guess is “too high” or “too low”. I used for loop to give the user exactly four opportunities to get the correct answer .Also ,I used break in if to stop the for loop if the guess is correct .
+    2. I added a 7th question that has multiple possible correct answers that are stored in an array and give the user 6 attempts to guess the correct answer. I declared an array of my family members names .then prompt the question to the use to guess the name of anyone of them .
+    the user input will entered to for loop with 6 iterations .then by using if statement I checked the user inter if it in the array or no by using array name.indexof(user inter) if it return a value of index item then the user inter is true .
+    3. I added score variable to store the correct answers then alert the user his score out of 7 .
+    
+
+
--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 let userName = prompt('What is your name?')
 alert ('welcome '+userName);
 alert('I\'m going to play a guessing game with you about me ');
-
+let score= 0;
+//Q1
 let knowMe= prompt('Do you think I worked for a company before?');
 
 knowMe= knowMe.toLowerCase();
@@ -17,12 +18,13 @@ else if (knowMe ==='no' || knowMe ==='n')
 {
 //console.log("correct");
 alert ('correct');
+score=score+1;
 }
 else {
     //console.log("omg!");
 alert ('omg!');
 }
-
+//Q2
 let footBall= prompt('Do I watch football?');
 
 footBall= footBall.toLowerCase();
@@ -37,12 +39,13 @@ else if (footBall ==='no' || footBall ==='n')
 {
 //console.log("correct");
 alert ('correct');
+score=score+1;
 }
 else {
     //console.log("omg!");
 alert ('omg!');
 }
-
+//Q3
 
 let knowUni= prompt('Did I graduate from JUST university?');
 
@@ -52,6 +55,7 @@ if (knowUni ==='yes' || knowUni ==='y')
 {
     //console.log("yes that is true");
     alert ('yes that is true');
+    score =score +1;
 
 }
 else if (knowUni ==='no' || knowUni ==='n')
@@ -63,6 +67,7 @@ else {
     //console.log("omg!");
 alert ('omg!');
 }
+//Q4
 
 let knowColor= prompt('Do I prefer the blue color?');
 
@@ -72,6 +77,7 @@ if (knowColor ==='yes' || knowColor==='y')
 {
     //console.log("yes that is true");
     alert ('yes that is true');
+    score=score+1;
 
 }
 else if (knowColor ==='no' || knowColor ==='n')
@@ -83,7 +89,7 @@ else {
     //console.log("omg!");
 alert ('omg!');
 }
-
+//Q5
 let know= prompt('Do you think I love my life?');
 
 know= know.toLowerCase();
@@ -92,7 +98,7 @@ if (know ==='yes' || know ==='y')
 {
    //console.log("correct");
    alert ('correct'); 
-    
+   score=score+1;
 
 }
 else if (know ==='no' || know ==='n')
@@ -104,7 +110,72 @@ else {
     //console.log("omg!");
 alert ('omg!');
 }
+//Q6 
+let knowFavNum= prompt('What is my favorite number?')
+knowFavNum=parseInt(knowFavNum);
+//console.log(knowFavNum)
+for ( let n=1 ; n<=4 ; n++)
+{
+    if (knowFavNum=== 5){
+        alert ('correct');
+
+     break;}
+     else if ( knowFavNum>5)
+    { alert("too high");}
+    else if (knowFavNum<5)
+    {
+        alert('too low');
+    }
+let attempt =4-n;
+knowFavNum= prompt('You have  ' + attempt +'attempts'+'\n What is my favorite number?');
+knowFavNum=parseInt(knowFavNum);
+
+}
+if (knowFavNum===5)
+    {
+        score=score+1;
+    }
+alert ('My favorite number is 5');
+
+//Q7
+let arrOfNames =['mamoun ','manal ','rahmah ','eman ','jana ','mohmmad '];
+//console.log(arrOfNames);
+let knowFamily=prompt('Guess the name of anyone of my family member?')
+knowFamily=knowFamily.toLowerCase();
+//console.log(knowFamily);
+for( let m=0;m <6;m++)
+{
+    if (arrOfNames.indexOf(knowFamily) !==-1)
+    {
+       break;
+
+    }
+    
+else 
+ {
+     let att = 5-m
+    knowFamily=prompt('You have '+ att + 'attempts'+' \n Guess the name of anyone of my family member?');
+    knowFamily=knowFamily.toLowerCase();
+    //console.log(knowFamily);  
+}
+}
+if (arrOfNames.indexOf(knowFamily) !==-1)
+    {
+        alert ('correct')
+        score =score+1;
+
+    }
+alert ( 'My family members are : '+ arrOfNames );
+
+alert ('Your score is '+ score +' out of 7')
+    if (score>=5)
+    {
+        alert ('You Know me well!');
+    }
+    else {
+        alert ('You don\'n know me well');
+    }
 alert ('Welcome to you again '+ userName);
+    
+       
 
-
-            
